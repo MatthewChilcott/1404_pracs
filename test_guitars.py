3469089d498503aff2a8a867af131f4a99fb2f4f
jc337863 <matthew.chilcott@my.jcu.edu.au>
2016-05-06 14:26:45 +1000

    Kivy

--- test_guitars.py
+++ test_guitars.py
@@ -1,7 +1,7 @@
 from guitars import Guitars
 
 user_input = ''
-guitar_tuple = ()
+guitars = []
 user_finished = False
 while user_finished == False:
     guitar_name = input("Name: ")
@@ -9,12 +9,20 @@ while user_finished == False:
         user_finished = True
     guitar_year = input("Year Made: ")
     guitar_price = input("Price: ")
-    guitar_list = (guitar_name, guitar_year, guitar_price)
-    guitar_tuple += guitar_list
-
-print(guitar_list)
-
+    new_guitar = Guitars(guitar_name, guitar_year, guitar_price)
+    guitars.append(new_guitar)
 gibson = Guitars("Gibson L-5 CES", 1922, 16035.40)
 fender = Guitars("Fender Stratocaster", 2014, 765.40)
 line = Guitars("Line 6 JTV-59", 2010, 1512.90)
 
+guitars.append(gibson)
+guitars.append(fender)
+guitars.append(line)
+
+for i in guitars:
+    print("""Guitar {}:
+    Name: {}
+    Year: {}
+    Cost: ${}
+    {} ({}) : ${} added.""".format(i, guitars[i][1], guitars[1], guitars[1], guitars[1], guitars[1], guitars[1]))
+    # {} ({}) : ${} added.""".format(i,guitar_name, guitar_year, guitar_price, guitar_name, guitar_year, guitar_price))
